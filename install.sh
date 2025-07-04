cddf406083b988ffe52b55c231cf1fea5bfedef6
sumit <sumit@duskbyte.com>
2025-07-04 11:35:47 +0530

    Add install shell script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TOKEN=$1
+SERVER_URL=$2
+
+if [ -z "$TOKEN" ] || [ -z "$SERVER_URL" ]; then
+  echo "Error: Token or Server URL missing."
+  echo "Usage: bash -s <AGENT_KEY> <SERVER_URL>"
+  exit 1
+fi
+
+echo "Downloading goAgent.deb package..."
+curl -fsSL https://github.com/deepakkundra1/dbchefs-public-scripts/releases/download/v1.0.0/goAgent.deb -o goAgent.deb
+
+echo "Installing go agent package"
+sudo dpkg -i goAgent.deb
+
+echo "Saving token and server URL to /etc/goAgent/config.yml"
+sudo mkdir -p /etc/goAgent
+
+# Write YAML config
+echo -e "token: \"$TOKEN\"\nserver_url: \"$SERVER_URL\"" | sudo tee /etc/goAgent/config.yml > /dev/null
+
+echo "Enabling and starting service"
+sudo systemctl daemon-reload
+sudo systemctl enable goAgent
+sudo systemctl start goAgent
+
+rm -f goAgent.deb
+
+echo "Go Agent installed and running!"
