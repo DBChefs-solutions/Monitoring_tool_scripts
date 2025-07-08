67dc3488eb13ba781af14b69ff822e6875110f8f
sumit <sumit@duskbyte.com>
2025-07-08 15:04:07 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ if [ -z "$TOKEN" ] || [ -z "$SERVER_URL" ]; then
   exit 1
 fi
 
-echo "Downloading goAgent.deb package..."
-curl -fsSL https://github.com/deepakkundra1/dbchefs-public-scripts/releases/download/v1.0.0/goAgent.deb -o goAgent.deb
+echo "Downloading goAgent_1.0.0_amd64.deb package..."
+curl -fsSL https://github.com/deepakkundra1/dbchefs-public-scripts/releases/download/v1.0.0/goAgent_1.0.0_amd64.deb -o goAgent_1.0.0_amd64.deb
 
 echo "Installing go agent package"
-sudo dpkg -i goAgent.deb
+sudo dpkg -i goAgent_1.0.0_amd64.deb
 
 echo "Saving token and server URL to /etc/goAgent/config.yml"
 sudo mkdir -p /etc/goAgent
